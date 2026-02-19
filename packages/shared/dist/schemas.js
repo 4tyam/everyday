13e3761cf8f6ff8202192aaa1f14ec5874143568
Aditya Atyam <atyamx@gmail.com>
2026-02-19 22:43:50 +0530

    nativewind

--- packages/shared/dist/schemas.js
+++ packages/shared/dist/schemas.js
@@ -5,6 +5,6 @@ const zod_1 = require("zod");
 exports.apiHealthSchema = zod_1.z.object({
     ok: zod_1.z.boolean(),
     service: zod_1.z.string(),
-    time: zod_1.z.string().datetime()
+    time: zod_1.z.string().datetime(),
 });
 //# sourceMappingURL=schemas.js.map
